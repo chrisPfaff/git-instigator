22158a6eb4aa66920ee9fc716b0533bdd6239d69
Chris Pfaff <chrispfaff10@gmail.com>
2020-01-22 21:48:55 -0600

    imported runtime

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app.js";
+import "regenerator-runtime/runtime";
 ReactDOM.render(<App />, document.getElementById("root"));
