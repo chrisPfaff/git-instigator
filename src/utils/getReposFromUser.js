377247e0cd29bc9a4d108a7c99fd296e10f96dea
Chris Pfaff <chrispfaff10@gmail.com>
2020-01-23 21:19:03 -0600

    changed util function to allow for more repo queries

---
+++ src/utils/getReposFromUser.js
@@ -0,0 +1,14 @@
+import "regenerator-runtime/runtime";
+
+const getReposFromUser = async user => {
+  const data = await fetch(`https://api.github.com/users/${user}`).then(res => {
+    return res.json();
+  });
+  const repoList = await fetch(`${data.repos_url}?per_page=150`).then(res => {
+    return res.json();
+  });
+  console.log(repoList);
+  return repoList;
+};
+
+export default getReposFromUser;
