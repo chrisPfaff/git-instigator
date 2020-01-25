d4b655213143dccab935021bffe0a3bcb21b1e70
Chris Pfaff <chrispfaff10@gmail.com>
2020-01-25 14:21:31 -0600

    added database

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,37 @@
 const express = require("express");
 const mailer = require("./mailer.js");
 require("dotenv").config();
-const mongo = require("mongodb");
-const MongoClient = require("mongodb").MongoClient;
+//const mongo = require("mongodb");
+//const MongoClient = require("mongodb").MongoClient;
 const mongoose = require("mongoose");
-
+const User = require("./model/User.js");
+const db = mongoose.connection;
 const url = process.env.MONGO_URI;
-
 const app = express();
 
 app.use(express.static("dist"));
 
-// MongoClient.connect(url, function(err, db) {
-//   if (err) throw err;
-//   console.log("Database created!");
-//   db.close();
-// });
-
 mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("connection open");
+});
+
+let user = new User({
+  name: "chris",
+  email: "10@gmail.com"
+});
+
+user.save(function(err, user) {
+  if (err) return console.error(err);
+  console.log("user created");
+});
+
+User.find({}, function(err, user) {
+  if (err) return handleError(err);
+  console.log(user);
+});
 
 app.get("/where", (req, res) => {
   mailer.transporter.sendMail(mailer.mailOptions, function(error, info) {
