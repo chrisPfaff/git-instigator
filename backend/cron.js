da0326358da2bd9c589db8b79ba7371e7f453cfc
Chris Pfaff <chrispfaff10@gmail.com>
2020-01-24 20:19:10 -0600

    added cron function for daily running

---
+++ backend/cron.js
@@ -0,0 +1,12 @@
+const CronJob = require("cron").CronJob;
+
+var job = new CronJob(
+  "* * * * * *",
+  function() {
+    console.log("You will see this message every second");
+  },
+  null,
+  true,
+  "America/Los_Angeles"
+);
+job.start();
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const CronJob = require("cron").CronJob;
 const app = express();
 
 app.use(express.static("dist"));
+
+var job = new CronJob(
+  "* * * * * *",
+  function() {
+    console.log("You will see this message every second");
+  },
+  null,
+  true,
+  "America/Los_Angeles"
+);
+job.start();
 
 app.get("/where", (req, res) => {
   res.send("where art though");
