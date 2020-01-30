b4909933f83be34ab5186640c339a3a828b97d09
Chris Pfaff <chrispfaff10@gmail.com>
2020-01-29 21:09:30 -0600

    figuring out if i should split up cron jobs

---
+++ backend/cronJobDatabase.js
@@ -0,0 +1,23 @@
+const CronJob = require("cron").CronJob;
+const mailer = require("./mailer.js");
+const User = require("./model/User.js");
+const getReposFromUser = require("./utils/getTimesFromRepos.js");
+const getTimesFromRepos = require("./utils/getTimesFromRepos.js");
+const checkRepoDate = require("./utils/checkRepoDate.js");
+
+const DatabaseJob = new CronJob(
+  "10 * * * * *",
+  function() {
+    console.log("database starting");
+    let find = User.find({}, function(err, user) {
+      if (err) return handleError(err);
+      console.log("user", user);
+      return user;
+    });
+  },
+  null,
+  true,
+  "America/Chicago"
+);
+
+module.exports = DatabaseJob;
