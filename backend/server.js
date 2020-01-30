6154dfd1073ac7a9262bc6ea7cb2acaedfa8674e
Chris Pfaff <chrispfaff10@gmail.com>
2020-01-29 21:22:47 -0600

    cleanup server

--- backend/server.js
+++ backend/server.js
@@ -9,45 +9,14 @@ const url = process.env.MONGO_URI;
 const app = express();
 const port = 3000;
 
-//serve static parcel app
 app.use(express.static("dist"));
 
-//connect to mongoose
 mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
 
-//open database
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
   console.log("connection open");
 });
-
-//!test user
-// let user = new User({
-//   name: "chris",
-//   email: "10@gmail.com"
-// });
-
-//!save user function
-// user.save(function(err, user) {
-//   if (err) return console.error(err);
-//   console.log("user created");
-// });
-
-//!user find
-// User.find({}, function(err, user) {
-//   if (err) return handleError(err);
-//   console.log(user);
-// });
-
-//!mailer
-// mailer.transporter.sendMail(mailer.mailOptions, function(error, info) {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log("Email sent: " + info.response);
-//   }
-// });
-// res.send("sent");
 
 app.get("/user", (req, res) => {
   console.log(req.query.user, req.query.email);
