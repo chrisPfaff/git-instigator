e5349ae5294ecd6001af79674f2a0eef6d4e2a27
Chris Pfaff <chrispfaff10@gmail.com>
2020-01-30 20:57:56 -0600

    added node schedule for email send

--- backend/cronJobEmail.js
+++ backend/cronJobEmail.js
@@ -1,6 +1,6 @@
-const CronJob = require("cron").CronJob;
+const schedule = require("node-schedule");
 const mailer = require("./mailer.js");
-//const User = require("./model/User.js");
+const User = require("./model/User.js");
 const user = require("./cronJobDatabase");
 const getReposFromUser = require("./utils/getTimesFromRepos.js");
 const getTimesFromRepos = require("./utils/getTimesFromRepos.js");
@@ -20,29 +20,47 @@ require("dotenv").config();
 //!cron job time
 //"30 18 * * *"
 
-const EmailJob = new CronJob(
-  "20 * * * * *",
-  function() {
-    console.log(user);
-    // find.exec().then(data => {
-    //   for (const key in data) {
-    //     console.log(data[key].name);
-    //     mailer.transporter.sendMail(
-    //       mailer.mailOptions(data[key].email, data[key].name),
-    //       function(error, info) {
-    //         if (error) {
-    //           console.log(error);
-    //         } else {
-    //           console.log("Email sent: " + info.response);
-    //         }
-    //       }
-    //     );
-    //   }
-    // });
-  },
-  null,
-  true,
-  "America/Chicago"
-);
+// const EmailJob = new CronJob(
+//   "20 * * * * *",
+//   function() {
+// find.exec().then(data => {
+//   for (const key in data) {
+//     console.log(data[key].name);
+//     mailer.transporter.sendMail(
+//       mailer.mailOptions(data[key].email, data[key].name),
+//       function(error, info) {
+//         if (error) {
+//           console.log(error);
+//         } else {
+//           console.log("Email sent: " + info.response);
+//         }
+//       }
+//     );
+//   }
+// });
+//   },
+//   null,
+//   true,
+//   "America/Chicago"
+// );
 
-EmailJob.start();
+const EmailJob = schedule.scheduleJob("20 * * * * *", function() {
+  console.log("email job");
+  // find.exec().then(data => {
+  //   for (const key in data) {
+  //     console.log(data[key].name);
+  //     mailer.transporter.sendMail(
+  //       mailer.mailOptions(data[key].email, data[key].name),
+  //       function(error, info) {
+  //         if (error) {
+  //           console.log(error);
+  //         } else {
+  //           console.log("Email sent: " + info.response);
+  //         }
+  //       }
+  //     );
+  //   }
+  // });
+});
+
+module.exports = EmailJob;
