bed7ab2ebc7fdddf1b7ec73639ece757450fd949
Chris Pfaff <chrispfaff10@gmail.com>
2020-02-01 21:44:56 -0600

    changed front end style

--- backend/cronJobDatabase.js
+++ backend/cronJobDatabase.js
@@ -8,44 +8,44 @@ const checkRepoDate = require("./utils/checkRepoDate.js");
 //! best way to pass data to multiple jobs
 let repoList;
 
-const FindJob = schedule.scheduleJob("20 * * * * *", function() {
-  let find = User.find({}, function(err, user) {
-    if (err) return handleError(err);
-    return user;
-  });
-  find.exec().then(data => {
-    for (const key in data) {
-      repoList = getReposFromUsers(data[key].name);
-    }
-  });
-});
+// const FindJob = schedule.scheduleJob("20 * * * * *", function() {
+//   let find = User.find({}, function(err, user) {
+//     if (err) return handleError(err);
+//     return user;
+//   });
+//   find.exec().then(data => {
+//     for (const key in data) {
+//       repoList = getReposFromUsers(data[key].name);
+//     }
+//   });
+// });
 
-const EmailJob = schedule.scheduleJob("30 * * * * *", function() {
-  repoList.then(data => {
-    console.log(data);
-  });
-  // repoList.then(data => {
-  //   console.log(data);
-  // });
-  // let find = User.find({}, function(err, user) {
-  //   if (err) return handleError(err);
-  //   return user;
-  // });
-  // find.exec().then(data => {
-  //   for (const key in data) {
-  //     console.log(data[key].name);
-  //     mailer.transporter.sendMail(
-  //       mailer.mailOptions(data[key].email, data[key].name),
-  //       function(error, info) {
-  //         if (error) {
-  //           console.log(error);
-  //         } else {
-  //           console.log("Email sent: " + info.response);
-  //         }
-  //       }
-  //     );
-  //   }
-  // });
-});
+// const EmailJob = schedule.scheduleJob("30 * * * * *", function() {
+//   repoList.then(data => {
+//     console.log(data);
+//   });
+// repoList.then(data => {
+//   console.log(data);
+// });
+// let find = User.find({}, function(err, user) {
+//   if (err) return handleError(err);
+//   return user;
+// });
+// find.exec().then(data => {
+//   for (const key in data) {
+//     console.log(data[key].name);
+//     mailer.transporter.sendMail(
+//       mailer.mailOptions(data[key].email, data[key].name),
+//       function(error, info) {
+//         if (error) {
+//           console.log(error);
+//         } else {
+//           console.log("Email sent: " + info.response);
+//         }
+//       }
+//     );
+//   }
+// });
+// });
 
-module.exports = EmailJob;
+//module.exports = EmailJob;
