eab9bbca93214817caf61952477c653b6544d35b
Chris Pfaff <chrispfaff10@gmail.com>
2020-02-03 19:23:41 -0600

    added new model

---
+++ backend/model/UserEmail.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let UserEmail = new Schema({
+  name: { type: String, required: true, max: 100 },
+  email: { type: String, required: true },
+  repo: { type: String, required: true }
+});
+
+module.exports = mongoose.model("UserEmail", UserEmail);
