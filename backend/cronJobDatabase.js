280da8558a7e73f11d46a25366285f6f81e05d1b
Chris Pfaff <chrispfaff10@gmail.com>
2020-02-03 20:36:14 -0600

    git instigator working by checking correct github need to change it to incorrect

--- backend/cronJobDatabase.js
+++ backend/cronJobDatabase.js
@@ -1,5 +1,7 @@
 const schedule = require("node-schedule");
 const mailer = require("./mailer.js");
+const mongoose = require("mongoose");
+const db = mongoose.connection;
 const User = require("./model/User.js");
 const UserEmail = require("./model/UserEmail.js");
 const getReposFromUsers = require("./utils/getReposFromUsers.js");
@@ -8,10 +10,22 @@ const checkRepoDate = require("./utils/checkRepoDate.js");
 //! best way to pass data to multiple jobs
 
 const FindJob = schedule.scheduleJob("20 * * * * *", function() {
+  //delete collection to add new users
+  db.dropCollection("useremails", function(err, result) {
+    if (err) {
+      console.log("error delete collection");
+    } else {
+      console.log("delete collection success");
+    }
+  });
+
+  //find users
   let find = User.find({}, function(err, user) {
     if (err) return handleError(err);
     return user;
   });
+
+  // iterate over users found in db
   find.exec().then(data => {
     for (const key in data) {
       let user = data[key].email;
@@ -35,32 +49,28 @@ const FindJob = schedule.scheduleJob("20 * * * * *", function() {
   });
 });
 
-// const EmailJob = schedule.scheduleJob("30 * * * * *", function() {
-//   repoList.then(data => {
-//     console.log(data);
-//   });
-// repoList.then(data => {
-//   console.log(data);
-// });
-// let find = User.find({}, function(err, user) {
-//   if (err) return handleError(err);
-//   return user;
-// });
-// find.exec().then(data => {
-//   for (const key in data) {
-//     console.log(data[key].name);
-//     mailer.transporter.sendMail(
-//       mailer.mailOptions(data[key].email, data[key].name),
-//       function(error, info) {
-//         if (error) {
-//           console.log(error);
-//         } else {
-//           console.log("Email sent: " + info.response);
-//         }
-//       }
-//     );
-//   }
-// });
-// });
+const EmailJob = schedule.scheduleJob("30 * * * * *", function() {
+  //find users who pushed to github today
+  let find = UserEmail.find({}, function(err, UserEmail) {
+    if (err) return handleError(err);
+    return UserEmail;
+  });
+  //send those users an email
+  find.exec().then(data => {
+    for (const key in data) {
+      console.log(data[key]);
+      mailer.transporter.sendMail(
+        mailer.mailOptions(data[key].email, data[key].name),
+        function(error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        }
+      );
+    }
+  });
+});
 
-//module.exports = EmailJob;
+module.exports = EmailJob;
