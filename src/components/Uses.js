51e7b40953c60117e1311b94d5fe3a2473a3ff52
Chris Pfaff <chrispfaff10@gmail.com>
2020-02-05 15:52:31 -0600

    added uses page

--- src/components/Uses.js
+++ src/components/Uses.js
@@ -1,5 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  DiJavascript1,
+  DiLess,
+  DiReact,
+  DiMongodb,
+  DiGitCompare,
+  DiGithubAlt,
+  DiNodejs,
+  DiNodejsSmall
+} from "react-icons/di";
 
 import "../styles/Uses.scss";
 
@@ -11,6 +21,43 @@ const Uses = () => {
           Back
         </Link>
       </li>
+      <div className="Uses_title">
+        <h1>A collection of the tech used on this App</h1>
+      </div>
+      <div className="Uses_content">
+        <ul className="Uses_description">
+          <DiJavascript1 />
+          <p>Javascript</p>
+        </ul>
+        <ul className="Uses_description">
+          <DiLess />
+          <p>LESS</p>
+        </ul>
+        <ul className="Uses_description">
+          <DiReact />
+          <p>React</p>
+        </ul>
+        <ul className="Uses_description">
+          <DiMongodb />
+          <p>DiMongodb</p>
+        </ul>
+        <ul className="Uses_description">
+          <DiGitCompare />
+          <p>Parcel</p>
+        </ul>
+        <ul className="Uses_description">
+          <DiGithubAlt />
+          <p>Githubs API</p>
+        </ul>
+        <ul className="Uses_description">
+          <DiNodejs />
+          <p>Node</p>
+        </ul>
+        <ul className="Uses_description">
+          <DiNodejsSmall />
+          <p>Node_cron, momento, nodemailer</p>
+        </ul>
+      </div>
     </div>
   );
 };
