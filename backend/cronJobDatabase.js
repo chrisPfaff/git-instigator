177b35462bb46b299fcffd291a47b710a2ebadb7
Chris Pfaff <chrispfaff10@gmail.com>
2020-02-06 21:30:41 -0600

    commented out brains to add styles

--- backend/cronJobDatabase.js
+++ backend/cronJobDatabase.js
@@ -19,59 +19,59 @@ const FindJob = schedule.scheduleJob("20 * * * * *", function() {
     }
   });
 
-  //find users
-  let find = User.find({}, function(err, user) {
-    if (err) return handleError(err);
-    return user;
-  });
+//   //find users
+//   let find = User.find({}, function(err, user) {
+//     if (err) return handleError(err);
+//     return user;
+//   });
 
-  // iterate over users found in db
-  find.exec().then(data => {
-    for (const key in data) {
-      let user = data[key].email;
-      let repoList = getReposFromUsers(data[key].name);
-      console.log(user);
-      repoList.then(data => {
-        data.forEach(item => {
-          if (checkRepoDate(item.updated_at)) {
-            let emailContents = new UserEmail({
-              name: item.owner.login,
-              email: user,
-              repo: item.name
-            });
-            emailContents.save(function(err, emailContents) {
-              if (err) throw err;
-              console.log("user email created");
-            });
-          }
-        });
-      });
-    }
-  });
-});
+//   // iterate over users found in db
+//   find.exec().then(data => {
+//     for (const key in data) {
+//       let user = data[key].email;
+//       let repoList = getReposFromUsers(data[key].name);
+//       console.log(user);
+//       repoList.then(data => {
+//         data.forEach(item => {
+//           if (checkRepoDate(item.updated_at)) {
+//             let emailContents = new UserEmail({
+//               name: item.owner.login,
+//               email: user,
+//               repo: item.name
+//             });
+//             emailContents.save(function(err, emailContents) {
+//               if (err) throw err;
+//               console.log("user email created");
+//             });
+//           }
+//         });
+//       });
+//     }
+//   });
+// });
 
-const EmailJob = schedule.scheduleJob("30 * * * * *", function() {
-  //find users who pushed to github today
-  let find = UserEmail.find({}, function(err, UserEmail) {
-    if (err) return handleError(err);
-    return UserEmail;
-  });
-  //send those users an email
-  find.exec().then(data => {
-    for (const key in data) {
-      let githubUser = data[key];
-      mailer.transporter.sendMail(mailer.mailOptions(githubUser), function(
-        error,
-        info
-      ) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log("Email sent: " + info.response);
-        }
-      });
-    }
-  });
-});
+// const EmailJob = schedule.scheduleJob("30 * * * * *", function() {
+//   //find users who pushed to github today
+//   let find = UserEmail.find({}, function(err, UserEmail) {
+//     if (err) return handleError(err);
+//     return UserEmail;
+//   });
+//   //send those users an email
+//   find.exec().then(data => {
+//     for (const key in data) {
+//       let githubUser = data[key];
+//       mailer.transporter.sendMail(mailer.mailOptions(githubUser), function(
+//         error,
+//         info
+//       ) {
+//         if (error) {
+//           console.log(error);
+//         } else {
+//           console.log("Email sent: " + info.response);
+//         }
+//       });
+//     }
+//   });
+// });
 
-module.exports = EmailJob;
+// module.exports = EmailJob;
