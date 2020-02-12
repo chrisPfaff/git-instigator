0379faaa5f83eaf690fc4424eb82a8d2d22018cc
Chris Pfaff <chrispfaff10@gmail.com>
2020-02-11 21:28:12 -0600

    trying to get repodate to work

--- backend/utils/checkRepoDate.js
+++ backend/utils/checkRepoDate.js
@@ -1,12 +1,13 @@
 const moment = require("moment");
 
 const checkRepoDate = repoDate => {
+  //console.log("repodate", repoDate);
   let todaysDate = moment()
     .utc()
     .format()
     .slice(0, 10);
-  console.log("repodate", repoDate.slice(0, 10));
-  console.log("real data", todaysDate);
+  //console.log("repodate", repoDate.slice(0, 10));
+  //console.log("real data", todaysDate);
   if (todaysDate === repoDate.slice(0, 10)) {
     return true;
   }
